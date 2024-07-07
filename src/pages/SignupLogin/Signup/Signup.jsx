45c235b07846c5635a41dc15ae8fc43c6461c62e
Akbar <akbarjonjorayev001@gmail.com>
2024-07-07 15:15:40 +0500

    way to home page from signup/login pages

--- src/pages/SignupLogin/Signup/Signup.jsx
+++ src/pages/SignupLogin/Signup/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 
 import Button from '../../../components/Button/Button'
@@ -10,10 +10,12 @@ import {
   createAccount,
   saveIDToLocalStorage,
 } from '../../../modules/account.module'
+import { loadFromLocalStorage } from '../../../js/localDB/localstorage'
 
 import '../SignupLogin.css'
 
 export default function Signup() {
+  const id = useRef(loadFromLocalStorage('aj_videos')?.id).current
   const [inputData, setInputData] = useState({
     name: '',
     username: '',
@@ -44,50 +46,57 @@ export default function Signup() {
         draggable
       />
       <div className="d_f_ce h_100">
-        <div className="con_bd_cl account_page_con list_y">
-          <div className="list_x d_f_jc_sb">
-            <h2>Signup</h2>
-            <Button
-              className="btn_cl"
-              onClick={() => goToHref('/account/login')}
-            >
-              Login
-            </Button>
+        <div className="list_y account_page_con">
+          <div className="con_bd_cl list_y">
+            <div className="list_x d_f_jc_sb">
+              <h2>Signup</h2>
+              <Button
+                className="btn_cl"
+                onClick={() => goToHref('/account/login')}
+              >
+                Login
+              </Button>
+            </div>
+            <hr />
+            <form className="list_y" onSubmit={createAcc} disabled={disabled}>
+              <Input
+                label="Name"
+                value={inputData.name}
+                autoFocus
+                onChange={(e) =>
+                  setInputData({ ...inputData, name: e.target.value })
+                }
+              />
+              <Input
+                label="Username"
+                value={inputData.username}
+                onChange={(e) =>
+                  setInputData({ ...inputData, username: e.target.value })
+                }
+              />
+              <Input
+                label="Password"
+                type="password"
+                value={inputData.password}
+                onChange={(e) =>
+                  setInputData({ ...inputData, password: e.target.value })
+                }
+              />
+              <Button
+                className="btn_cl"
+                disabled={
+                  !inputData.name || !inputData.username || !inputData.password
+                }
+              >
+                {disabled ? 'Creating account' : 'Create account'}
+              </Button>
+            </form>
           </div>
-          <hr />
-          <form className="list_y" onSubmit={createAcc} disabled={disabled}>
-            <Input
-              label="Name"
-              value={inputData.name}
-              autoFocus
-              onChange={(e) =>
-                setInputData({ ...inputData, name: e.target.value })
-              }
-            />
-            <Input
-              label="Username"
-              value={inputData.username}
-              onChange={(e) =>
-                setInputData({ ...inputData, username: e.target.value })
-              }
-            />
-            <Input
-              label="Password"
-              type="password"
-              value={inputData.password}
-              onChange={(e) =>
-                setInputData({ ...inputData, password: e.target.value })
-              }
-            />
-            <Button
-              className="btn_cl"
-              disabled={
-                !inputData.name || !inputData.username || !inputData.password
-              }
-            >
-              {disabled ? 'Creating account' : 'Create account'}
+          {id && (
+            <Button className="btn_cl" onClick={() => goToHref('/')}>
+              Home page
             </Button>
-          </form>
+          )}
         </div>
       </div>
     </>
