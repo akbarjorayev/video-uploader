639804ccae52b7cb2641cbc2ba3172eb56d11f57
Akbar <akbarjonjorayev001@gmail.com>
2024-07-07 14:12:50 +0500

    app checker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { checkApplication } from './js/utils/checker'
 
 import './css/App.css'
 import 'react-toastify/dist/ReactToastify.css'
@@ -8,6 +10,11 @@ const Login = React.lazy(() => import('./pages/SignupLogin/Login/Login'))
 const Signup = React.lazy(() => import('./pages/SignupLogin/Signup/Signup'))
 
 export default function App() {
+  const checkApp = useCallback(() => {
+    checkApplication()
+  }, [])
+  checkApp()
+
   return (
     <>
       <BrowserRouter>
